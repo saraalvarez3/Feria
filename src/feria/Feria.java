6323331d4a87e57cf639d00f94a5c4d8e7788971
saraalvarez3 <sara.alvarez@mariaelvinia.edu.co>
2019-10-10 00:35:28 -0500

    version 1.0
    creaciones de clase

---
+++ src/feria/Feria.java
@@ -0,0 +1,13 @@
+package feria;
+
+import java.util.Date;
+
+public class Feria {
+
+    private  int numerodePersonas; 
+    private Date fechadeinicio;
+    private String nombredelorganizador;
+    private boolean tienecaminatas;
+    private int numerodesillas;
+
+}
